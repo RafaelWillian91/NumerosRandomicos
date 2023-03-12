8810ccc8d130e483201ba7dcafc5f60e6422fc25
Rafa <rafa.willian91@gmail.com>
2023-03-12 04:07:57 -0300

    Identificador Unico UUID

---
+++ src/UUID/Main.java
@@ -0,0 +1,16 @@
+package UUID;
+
+import java.util.UUID;
+
+public class Main {
+
+    //Para Gerar um identificador Randomico e único podemos usar a Classe UUID
+
+    public static void main(String[] args) {
+
+        UUID example = UUID.randomUUID();
+        System.out.println(example);
+
+    }
+
+}
